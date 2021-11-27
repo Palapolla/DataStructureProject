5fca0d4cd434c565a505622d8886b4e94a4c794a
premmm123 <poomsakppk@gmail.com>
2021-11-28 01:59:42 +0700

    add check in interface

--- menu.py
+++ menu.py
@@ -1,8 +1,20 @@
 from tkinter import *
+import datetime
 
 
 def show_frame(frame):
     frame.tkraise()
+
+
+def room_selected_command(event):
+    pass
+    # print(roomSelected.get())
+
+
+def updateTime():
+    now = datetime.datetime.now()
+    timeLabel.config(text=now.strftime("%Y-%m-%d %H:%M:%S"))
+    timeLabel.after(1000, updateTime)
 
 
 root = Tk()
@@ -68,6 +80,15 @@ exitBtn = Button(menu_Frame,
                  border=5,
                  command=quit
                  ).pack(padx=10, pady=10)
+now = datetime.datetime.now()
+timeLabel = Label(menu_Frame,
+                  text=now.strftime("%Y-%m-%d %H:%M:%S"),
+                  font="ariel 20",
+                  pady=20,
+                  width=20,
+                  border=5,)
+timeLabel.place(x=950, y=650)
+timeLabel.after(1000, updateTime)
 
 # ======================= 1.Check in Frame Code ================
 checkinLabel = Label(checkin_Frame,
@@ -77,6 +98,83 @@ checkinLabel = Label(checkin_Frame,
                      width=20,
                      border=5
                      ).pack(padx=10, pady=10)
+
+nameLabel = Label(checkin_Frame,
+                  text="Enter name :",
+                  font="Times 20",
+                  width=20,
+                  border=5
+                  ).place(x=20, y=100)
+
+nameEntry = Entry(checkin_Frame,
+                  font="Times 20",
+                  width=40,
+                  border=5
+                  ).place(x=400, y=100)
+
+roomLabel = Label(checkin_Frame,
+                  text="Enter room type :",
+                  font="Times 20",
+                  width=20,
+                  border=5
+                  ).place(x=20, y=200)
+
+roomType = ["Normal", "Deluxe", "Floor"]
+roomSelected = StringVar()
+roomSelected.set(roomType[0])
+roomOptions = OptionMenu(checkin_Frame, roomSelected,
+                         *roomType, command=room_selected_command)
+roomOptions.place(x=340, y=200)
+roomOptions.config(font="Times 18")
+
+amountDayLabel = Label(checkin_Frame,
+                       text="How long ? (days) :",
+                       font="Times 20",
+                       width=20,
+                       border=5
+                       ).place(x=500, y=200)
+
+days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+daySelected = IntVar()
+daySelected.set(days[0])
+dayOption = OptionMenu(checkin_Frame, daySelected,
+                       *days, command=room_selected_command)
+dayOption.place(x=800, y=200)
+dayOption.config(font="Times 18")
+
+dateLabel = Label(checkin_Frame,
+                  text="Enter date (yy-dd-mm):",
+                  font="Times 20",
+                  width=20,
+                  border=5
+                  ).place(x=20, y=300)
+
+dateEntry = Entry(checkin_Frame,
+                  font="Times 20",
+                  width=30,
+                  border=5
+                  ).place(x=400, y=300)
+
+phoneLabel = Label(checkin_Frame,
+                   text="Enter phone number:",
+                   font="Times 20",
+                   width=20,
+                   border=5
+                   ).place(x=20, y=400)
+
+phoneEntry = Entry(checkin_Frame,
+                   font="Times 20",
+                   width=30,
+                   border=5
+                   ).place(x=400, y=400)
+
+checkin_submit_btn = Button(checkin_Frame,
+                            text="SUBMIT",
+                            font="Times 15",
+                            pady=20,
+                            width=20,
+                            border=5
+                            ).place(x=500, y=500)
 
 backBtn = Button(checkin_Frame,
                  text="BACK TO MENU",
