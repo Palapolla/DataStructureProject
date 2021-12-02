3a5fb8260b361955c62bfd85153e35a83df436e5
premmm123 <poomsakppk@gmail.com>
2021-12-02 16:37:56 +0700

    complete checkin v1.00

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 from tkinter import *
+from tkcalendar import *
+from tkinter import messagebox
 import datetime
+import checkin
 
-# test
+
 def show_frame(frame):
     frame.tkraise()
-        
 
 
-def room_selected_command(event):
-    pass
-    # print(roomSelected.get())
+def room_id_selected_command():
+
+    handleError, roomAvarible = checkin.findAvailableRoom(roomTypeSelected.get(
+    ), dateCheckin.get_date(), dateCheckout.get_date())
+
+    if handleError != "":
+        messagebox.showinfo("Status", handleError)
+    else:
+        top = Toplevel(root)
+        top.geometry("300x300")
+        roomOptionsA = OptionMenu(top, roomSelectedVariable, *roomAvarible)
+        roomOptionsA.pack()
+
+        closeBtn = Button(top, text="close", command=top.destroy).pack(pady=30)
+
+
+def handleSubmitData():
+    handleError = checkin.writeData(nameEntry.get(), sureNameEntry.get(), phoneEntry.get(
+    ), roomTypeSelected.get(), roomSelectedVariable.get(), dateCheckin.get_date(), dateCheckout.get_date())
+
+    messagebox.showinfo("Status", handleError)
+
+
+def clear_input_command():
+    nameEntry.delete(0, 'end')
+    sureNameEntry.delete(0, 'end')
+    phoneEntry.delete(0, 'end')
+    roomTypeSelected.set("Normal")
+    roomSelectedVariable.set("\"Not selected\"")
+    dateCheckin.set_date(datetime.date.today())
+    dateCheckout.set_date(datetime.date.today())
 
 
 def updateTime():
@@ -18,7 +48,7 @@ def updateTime():
     timeLabel.after(1000, updateTime)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
     root = Tk()
 
@@ -28,7 +58,7 @@ if __name__=='__main__':
     width_of_window = 1280
     height_of_window = 720
     root.geometry("%dx%d+%d+%d" % (width_of_window, height_of_window,
-                ((root.winfo_screenwidth()/2)-(width_of_window/2)), ((root.winfo_screenheight()/2)-(height_of_window/2))))
+                                   ((root.winfo_screenwidth()/2)-(width_of_window/2)), ((root.winfo_screenheight()/2)-(height_of_window/2))))
     root.title("HOTEL MANAGEMENT")
     root.resizable(width=False, height=False)
 
@@ -59,159 +89,182 @@ if __name__=='__main__':
                         ).pack()
 
     showGuestBtn = Button(menu_Frame,
-                        text="👥GUEST LIST",
-                        font="Times 15",
-                        width=20,
-                        background='#8BEEFF',
-                        activebackground='#2AD6EC',
-                        relief='flat',
-                        command=lambda: show_frame(guestList_Frame)
-                        ).pack()
+                          text="👥GUEST LIST",
+                          font="Times 15",
+                          width=20,
+                          background='#8BEEFF',
+                          activebackground='#2AD6EC',
+                          relief='flat',
+                          command=lambda: show_frame(guestList_Frame)
+                          ).pack()
 
     checkOutBtn = Button(menu_Frame,
-                        text="◀CHECK OUT",
-                        font="Times 15",
-                        width=20,
-                        background='#8BEEFF',
-                        activebackground='#2AD6EC',
-                        relief='flat',
-                        command=lambda: show_frame(checkout_Frame)
-                        ).pack()
+                         text="◀CHECK OUT",
+                         font="Times 15",
+                         width=20,
+                         background='#8BEEFF',
+                         activebackground='#2AD6EC',
+                         relief='flat',
+                         command=lambda: show_frame(checkout_Frame)
+                         ).pack()
 
     exitBtn = Button(menu_Frame,
-                    text="EXIT",
-                    font="Times 15",
-                    width=20,
-                    background='#8BEEFF',
-                    activebackground='#2AD6EC',
-                    relief='flat',
-                    command=quit
-                    ).pack()
+                     text="EXIT",
+                     font="Times 15",
+                     width=20,
+                     background='#8BEEFF',
+                     activebackground='#2AD6EC',
+                     relief='flat',
+                     command=quit
+                     ).pack()
     now = datetime.datetime.now()
     timeLabel = Label(root,
-                    text=now.strftime("%Y-%m-%d %H:%M:%S"),
-                    font="ariel 10",
-                    pady=20,
-                    width=20,
-                    border=5,)
+                      text=now.strftime("%Y-%m-%d %H:%M:%S"),
+                      font="ariel 10",
+                      pady=20,
+                      width=20,
+                      border=5,)
     timeLabel.place(x=1100, y=650)
     timeLabel.after(1000, updateTime)
 
     # ======================= 1.Check in Frame Code ================
     checkinLabel = Label(checkin_Frame,
-                        text="CHECK IN",
-                        font="Times 20",
-                        pady=20,
-                        width=20,
-                        border=5
-                        ).pack(padx=10, pady=10)
+                         text="CHECK IN",
+                         font="Times 20",
+                         pady=20,
+                         width=20,
+                         border=5
+                         ).pack(padx=10, pady=10)
 
     nameLabel = Label(checkin_Frame,
-                    text="Enter name :",
-                    font="Times 20",
-                    width=20,
-                    border=5
-                    ).place(x=20, y=100)
+                      text="Enter name :",
+                      font="Times 20",
+                      width=0,
+                      border=5
+                      ).place(x=50, y=100)
 
     nameEntry = Entry(checkin_Frame,
-                    font="Times 20",
-                    width=40,
-                    border=5
-                    ).place(x=400, y=100)
+                      font="Times 20",
+                      width=15,
+                      border=5
+                      )
+    nameEntry.place(x=220, y=100)
 
-    roomLabel = Label(checkin_Frame,
-                    text="Enter room type :",
-                    font="Times 20",
-                    width=20,
-                    border=5
-                    ).place(x=20, y=200)
+    sureNameLabel = Label(checkin_Frame,
+                          text="Surename :",
+                          font="Times 20",
+                          width=20,
+                          border=5
+                          ).place(x=500, y=100)
 
-    roomType = ["Normal", "Deluxe", "Floor"]
-    roomSelected = StringVar()
-    roomSelected.set(roomType[0])
-    roomOptions = OptionMenu(checkin_Frame, roomSelected,
-                            *roomType, command=room_selected_command)
-    roomOptions.place(x=340, y=200)
-    roomOptions.config(font="Times 18")
-
-    amountDayLabel = Label(checkin_Frame,
-                        text="How long ? (days) :",
-                        font="Times 20",
-                        width=20,
-                        border=5
-                        ).place(x=500, y=200)
-
-    days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    daySelected = IntVar()
-    daySelected.set(days[0])
-    dayOption = OptionMenu(checkin_Frame, daySelected,
-                        *days, command=room_selected_command)
-    dayOption.place(x=800, y=200)
-    dayOption.config(font="Times 18")
-
-    dateLabel = Label(checkin_Frame,
-                    text="Enter date (yy-dd-mm):",
-                    font="Times 20",
-                    width=20,
-                    border=5
-                    ).place(x=20, y=300)
-
-    dateEntry = Entry(checkin_Frame,
-                    font="Times 20",
-                    width=30,
-                    border=5
-                    ).place(x=400, y=300)
+    sureNameEntry = Entry(checkin_Frame,
+                          font="Times 20",
+                          width=15,
+                          border=5
+                          )
+    sureNameEntry.place(x=750, y=100)
 
     phoneLabel = Label(checkin_Frame,
-                    text="Enter phone number:",
-                    font="Times 20",
-                    width=20,
-                    border=5
-                    ).place(x=20, y=400)
+                       text="Enter phone number:",
+                       font="Times 20",
+                       width=20,
+                       border=5
+                       ).place(x=20, y=180)
 
     phoneEntry = Entry(checkin_Frame,
-                    font="Times 20",
-                    width=30,
-                    border=5
-                    ).place(x=400, y=400)
+                       font="Times 20",
+                       width=30,
+                       border=5
+                       )
+    phoneEntry.place(x=350, y=180)
+
+    roomLabel = Label(checkin_Frame,
+                      text="Enter room type :",
+                      font="Times 20",
+                      width=0,
+                      border=5
+                      ).place(x=50, y=260)
+
+    roomType = ["Normal", "Deluxe", "Twin"]
+    roomTypeSelected = StringVar()
+    roomTypeSelected.set(roomType[0])
+    roomOptions = OptionMenu(checkin_Frame, roomTypeSelected,
+                             *roomType)
+    roomOptions.place(x=340, y=260)
+    roomOptions.config(font="Times 18")
+
+    labelDateCheckin = Label(checkin_Frame,
+                             text="Date check in :",
+                             font="Times 20",
+                             width=0,
+                             border=5
+                             ).place(x=50, y=340)
+
+    dateCheckin = DateEntry(checkin_Frame, selectmode='day',
+                            year=now.year, month=now.month, day=now.day)
+    dateCheckin.place(x=290, y=350)
+
+    labelDateCheckout = Label(checkin_Frame,
+                              text="Date check out :",
+                              font="Times 20",
+                              width=0,
+                              border=5
+                              ).place(x=500, y=340)
+
+    dateCheckout = DateEntry(checkin_Frame, selectmode='day',
+                             year=now.year, month=now.month, day=now.day)
+    dateCheckout.place(x=740, y=350)
+
+    pleaseSelectRoomLabel = Label(checkin_Frame,
+                                  text="Select room :",
+                                  font="Times 20",
+                                  width=0,
+                                  border=5
+                                  ).place(x=50, y=420)
+
+    roomSelectedVariable = StringVar()
+    roomSelectedVariable.set("\"Not selected\"")
+    selectedRoomEntry = Entry(checkin_Frame, state=DISABLED,
+                              textvariable=roomSelectedVariable,
+                              font="Times 20", width=14,
+                              border=5
+                              ).place(x=220, y=420)
+
+    selectRoomBtn = Button(checkin_Frame, text="Sel Room",
+                           font="Times 15", command=room_id_selected_command).place(x=500, y=420)
+
+    clearInputBtn = Button(checkin_Frame, text="Clear input",
+                           font="Times 15", command=clear_input_command).place(x=565, y=520)
 
     checkin_submit_btn = Button(checkin_Frame,
                                 text="SUBMIT",
                                 font="Times 15",
                                 pady=20,
                                 width=20,
-                                border=5
-                                ).place(x=500, y=500)
-
-    # backBtn = Button(checkin_Frame,
-    #                 text="BACK TO MENU",
-    #                 font="Times 15",
-    #                 pady=20,
-    #                 width=20,
-    #                 border=5,
-    #                 command=lambda: show_frame(menu_Frame)
-    #                 ).place(x=10, y=630)
+                                border=5,
+                                command=handleSubmitData
+                                ).place(x=500, y=600)
 
     # ======================= 2.SHOW GUEST LIST Frame Code ================
     guestListLabel = Label(guestList_Frame,
-                        text="GUEST LIST",
-                        font="Times 20",
-                        pady=20,
-                        width=20,
-                        border=5
-                        ).pack(side='top')
+                           text="GUEST LIST",
+                           font="Times 20",
+                           pady=20,
+                           width=20,
+                           border=5
+                           ).pack(side='top')
 
-    table_Frame = LabelFrame(guestList_Frame, 
-                            text="Data"
-                            ).pack(fill='both')
+    table_Frame = LabelFrame(guestList_Frame,
+                             text="Data"
+                             ).pack(fill='both')
 
-    table_scrollbarx = Scrollbar(guestList_Frame, 
-                                orient='horizontal')
+    table_scrollbarx = Scrollbar(guestList_Frame,
+                                 orient='horizontal')
     table_scrollbarx.pack(side=BOTTOM, fill=X)
     # y
-    table_scrollbary = Scrollbar(guestList_Frame, 
-                                orient='vertical'
-                                ).pack(side=RIGHT, fill=Y)
+    table_scrollbary = Scrollbar(guestList_Frame,
+                                 orient='vertical'
+                                 ).pack(side=RIGHT, fill=Y)
 
     # backBtn = Button(guestList_Frame,
     #                 text="BACK TO MENU",
@@ -224,12 +277,12 @@ if __name__=='__main__':
 
     # ======================= 3.CHECK OUT Frame Code ================
     checkoutLabel = Label(checkout_Frame,
-                        text="CHECK OUT",
-                        font="Times 20",
-                        pady=20,
-                        width=20,
-                        border=5
-                        ).pack(padx=10, pady=10)
+                          text="CHECK OUT",
+                          font="Times 20",
+                          pady=20,
+                          width=20,
+                          border=5
+                          ).pack(padx=10, pady=10)
 
     # backBtn = Button(checkout_Frame,
     #                 text="BACK TO MENU",
@@ -239,7 +292,6 @@ if __name__=='__main__':
     #                 border=5,
     #                 command=lambda: show_frame(menu_Frame)
     #                 ).place(x=10, y=630)
-
 
     show_frame(menu_Frame)
 
