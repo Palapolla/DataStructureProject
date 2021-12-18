e716faeaded3bb8dfcede6b7d540ef68df725338
premmm123 <poomsakppk@gmail.com>
2021-12-18 14:42:21 +0700

    add bi-search, bbsort

--- main.py
+++ main.py
@@ -224,68 +224,23 @@ def deleteData(roomID):
 
 
 def handleSubmitCheckout():
+    if not checkoutEntry.get().isdigit():
+        return messagebox.showinfo("Status", "please enter only number")
+
     global data_ls
+    all_id = []
 
-    if N01.get():
-        deleteData("N01")
+    data = read_Json("Data.json")
+    for room in data:
+        if room != "LastID":
+            for items in data[room]["bookingData"]:
+                all_id.append(items["id"])
+    all_id = bubbleSort(all_id)
 
-        messagebox.showinfo("Status", 'Successfull')
-    if N02.get():
-        deleteData("N02")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if N03.get():
-        deleteData("N03")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if N04.get():
-        deleteData("N04")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if N05.get():
-        deleteData("N05")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if D01.get():
-        deleteData("D01")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if D02.get():
-        deleteData("D02")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if D03.get():
-        deleteData("D03")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if D04.get():
-        deleteData("D04")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if D05.get():
-        deleteData("D05")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if T01.get():
-        deleteData("T01")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if T02.get():
-        deleteData("T02")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if T03.get():
-        deleteData("T03")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if T04.get():
-        deleteData("T04")
-
-        messagebox.showinfo("Status", 'Successfull')
-    if T05.get():
-        deleteData("T05")
-
-        messagebox.showinfo("Status", 'Successfull')
+    if binarySearch(all_id, 0, len(all_id)-1, int(checkoutEntry.get())):
+        messagebox.showinfo("Status", "ยังไม่เสร็จ เดี๋ยวมาทำ")
+    else:
+        messagebox.showinfo("Status", "ID not found :(")
 
     data_ls = update_data()
     update_table()
@@ -304,6 +259,36 @@ def update_table():
                      index=row,
                      values=ele)
         row += 1
+
+
+def binarySearch(arr, l, r, x):
+    # result = binarySearch(arr, 0, len(arr)-1, target)
+    if r >= l:
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return True
+        elif arr[mid] > x:
+            return binarySearch(arr, l, mid-1, x)
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+    else:
+        return False
+
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+def ExitApplication():
+    MsgBox = messagebox.askquestion(
+        'Exit Application', 'Are you sure you want to exit the application', icon='warning')
+    if MsgBox == 'yes':
+        root.destroy()
 
 
 if __name__ == '__main__':
@@ -373,7 +358,7 @@ if __name__ == '__main__':
                      background='#40dead',
                      activebackground='#40dead',
                      relief='flat',
-                     command=quit
+                     command=ExitApplication
                      ).pack()
     now = datetime.datetime.now()
     timeLabel = Label(root,
@@ -596,175 +581,28 @@ if __name__ == '__main__':
                           border=5
                           ).pack(padx=10, pady=10)
 
-    normalLabel = Label(checkout_Frame,
-                        text="Normal room",
-                        font="Cascadia 20",
-                        background='#80dead',
-                        width=0,
-                        border=5
-                        ).place(x=50, y=100)
+    checkoutLabel2 = Label(checkout_Frame,
+                           text="Enter ID : ",
+                           font="Cascadia 50",
+                           background='#80dead'
+                           ).place(x=200, y=260)
 
-    N01 = BooleanVar()
-    N01Btn = Checkbutton(checkout_Frame,
-                         text="N01",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=N01
-                         ).place(x=50, y=175)
-    N02 = BooleanVar()
-    N02Btn = Checkbutton(checkout_Frame,
-                         text="N02",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=N02
-                         ).place(x=250, y=175)
-    N03 = BooleanVar()
-    N03Btn = Checkbutton(checkout_Frame,
-                         text="N03",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=N03
-                         ).place(x=450, y=175)
-    N04 = BooleanVar()
-    N04Btn = Checkbutton(checkout_Frame,
-                         text="N04",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=N04
-                         ).place(x=650, y=175)
-    N05 = BooleanVar()
-    N05Btn = Checkbutton(checkout_Frame,
-                         text="N05",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=N05
-                         ).place(x=850, y=175)
-
-    deluxeLabel = Label(checkout_Frame,
-                        text="Deluxe room",
-                        font="Cascadia 20",
-                        background='#80dead',
-                        width=0,
-                        border=5
-                        ).place(x=50, y=250)
-    D01 = BooleanVar()
-    D01Btn = Checkbutton(checkout_Frame,
-                         text="D01",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=D01
-                         ).place(x=50, y=325)
-    D02 = BooleanVar()
-    D02Btn = Checkbutton(checkout_Frame,
-                         text="D02",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=D02
-                         ).place(x=250, y=325)
-    D03 = BooleanVar()
-    D03Btn = Checkbutton(checkout_Frame,
-                         text="D03",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=D03
-                         ).place(x=450, y=325)
-    D04 = BooleanVar()
-    D04Btn = Checkbutton(checkout_Frame,
-                         text="D04",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=D04
-                         ).place(x=650, y=325)
-    D05 = BooleanVar()
-    D05Btn = Checkbutton(checkout_Frame,
-                         text="D05",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=D05
-                         ).place(x=850, y=325)
-
-    twinLabel = Label(checkout_Frame,
-                      text="Twin room",
-                      font="Cascadia 20",
-                      background='#80dead',
-                      width=0,
-                      border=5,
-                      ).place(x=50, y=400)
-    T01 = BooleanVar()
-    T01Btn = Checkbutton(checkout_Frame,
-                         text="T01",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=T01
-                         ).place(x=50, y=475)
-    T02 = BooleanVar()
-    T02Btn = Checkbutton(checkout_Frame,
-                         text="T02",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=T02
-                         ).place(x=250, y=475)
-    T03 = BooleanVar()
-    T03Btn = Checkbutton(checkout_Frame,
-                         text="T03",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=T03
-                         ).place(x=450, y=475)
-    T04 = BooleanVar()
-    T04Btn = Checkbutton(checkout_Frame,
-                         text="T04",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=T04
-                         ).place(x=650, y=475)
-    T05 = BooleanVar()
-    T05Btn = Checkbutton(checkout_Frame,
-                         text="T05",
-                         font="Cascadia 15",
-                         pady=10,
-                         width="10",
-                         border=5,
-                         variable=T05
-                         ).place(x=850, y=475)
+    checkoutEntry = Entry(checkout_Frame,
+                          font="Cascadia 50",
+                          width=4,
+                          border=5
+                          )
+    checkoutEntry.place(x=500, y=260)
 
     checkoutBtn = Button(checkout_Frame,
                          text='Check out',
                          font='Times 15',
                          pady=20,
                          width=20,
-                         border=10,
+                         border=3,
                          command=handleSubmitCheckout
                          ).place(x=400, y=600)
 
-    show_frame(menu_Frame)
+    show_frame(checkin_Frame)
 
     root.mainloop()
